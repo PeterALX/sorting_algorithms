0466644c8cfa7ebcecd76ceb452c8d5ee435e3f9
PeterALX <quaypeter25@gmail.com>
2023-01-25 09:06:00 +0000

    add

--- tests.c
+++ tests.c
@@ -10,12 +10,13 @@
 int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array2[] = {1};
+    size_t n = sizeof(array2) / sizeof(array2[0]);
 
-    print_array(array, n);
+    print_array(array2, n);
     printf("\n");
-    bubble_sort(array, n);
+    bubble_sort(array2, n);
     printf("\n");
-    print_array(array, n);
+    print_array(array2, n);
     return (0);
 }
