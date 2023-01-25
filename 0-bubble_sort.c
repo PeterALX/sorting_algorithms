4ec6373a461bdecafa18842f3d8426810d877a60
PeterALX <quaypeter25@gmail.com>
2023-01-25 08:58:40 +0000

    add

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,3 +29,17 @@ void bubble_sort(int *array, size_t size)
 			return;
 	}
 }
+
+/**
+ * swap - swap the value of two indices in an array
+ * @arr: the array
+ * @idx1: index 1 
+ * @idx2: index 2
+ */
+void swap(int *arr, int idx1, int idx2)
+{
+	int tmp = arr[idx1];
+
+	arr[idx1] = arr[idx2];
+	arr[idx2] = tmp;
+}
